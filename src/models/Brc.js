313a133c386d01b6c3976569a77d01d78a071b36
unknown <effiejohnortega@gmail.com>
2026-03-03 09:16:58 +0800

    latest

--- src/models/Brc.js
+++ src/models/Brc.js
@@ -3,40 +3,19 @@ const slugify = require("slugify");
 
 const BrcSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    slug: {
-      type: String,
-      unique: true,
-      index: true,
-    },
-    statement: {
-      type: String,
-      required: true,
-    },
-    coverImage: {
-      type: String,
-    },
-    images: {
-      type: [String],
-      default: [],
-    },
+    name: { type: String, required: true, trim: true },
+    slug: { type: String, unique: true, index: true },
+    statement: { type: String, required: true },
+    coverImage: { type: String },
+    images: { type: [String], default: [] },
   },
   { timestamps: true }
 );
 
-// IMPORTANT: use function(next) not arrow function, so `this` works
-BrcSchema.pre("save", function (next) {
-  try {
-    if (this.isModified("name") || this.isNew) {
-      this.slug = slugify(this.name, { lower: true, strict: true });
-    }
-    return next();
-  } catch (err) {
-    return next(err);
+// No next() needed
+BrcSchema.pre("save", function () {
+  if (this.isModified("name") || this.isNew) {
+    this.slug = slugify(this.name, { lower: true, strict: true });
   }
 });
 
